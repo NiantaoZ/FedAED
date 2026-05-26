4fdac70ffcffd143846b68fa2d1de1859751309a
NiantaoZ <155046557+NiantaoZ@users.noreply.github.com>
2026-05-26 10:35:26 +0800

    Add files via upload

---
+++ FedAED-main/FedAED/features/simulation_features/crisis-mmd/run_mm.sh
@@ -0,0 +1,7 @@
+for alpha in 1.0; do
+    for mm_rate in 0.1 0.2 0.3 0.4 0.5; do
+        #python3 simulation_feature.py --alpha $alpha --en_missing_modality --missing_modailty_rate $mm_rate
+        #python3 simulation_feature.py --alpha $alpha --en_missing_label --missing_label_rate $mm_rate
+        python3 simulation_feature.py --alpha $alpha --en_label_nosiy --label_nosiy_level $mm_rate --en_missing_label --missing_label_rate $mm_rate --en_missing_modality --missing_modailty_rate $mm_rate
+    done
+done
---
+++ FedAED-main/FedAED/features/simulation_features/meld/run_mm.sh
@@ -0,0 +1,5 @@
+for mm_rate in 0.1 0.2 0.3 0.4 0.5; do
+    #taskset 50 python3 simulation_feature.py --en_missing_modality --missing_modailty_rate $mm_rate
+    #taskset 50 python3 simulation_feature.py --en_missing_label --missing_label_rate $mm_rate
+    taskset 50 python3 simulation_feature.py --en_label_nosiy --label_nosiy_level $mm_rate --en_missing_label --missing_label_rate $mm_rate --en_missing_modality --missing_modailty_rate $mm_rate
+done
